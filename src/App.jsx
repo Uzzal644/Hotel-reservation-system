84650468d11a7ccfd6257845834276626131a865
Ujjwal Lamichhane <47378416+Uzzal644@users.noreply.github.com>
2024-01-28 20:45:57 +0545

    frontend design and layout finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Homescreen from './screens/Homescreen'
+// import React from "react"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import ExistingRooms from "./components/room/ExistingRooms"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/home/Home"
+import EditRoom from "./components/room/EditRoom"
+import AddRoom from "./components/room/AddRoom"
+import NavBar from "./components/layout/NavBar"
+import Footer from "./components/layout/Footer"
+import RoomListing from "./components/room/RoomListing"
+import Admin from "./components/admin/Admin"
+import Checkout from "./components/booking/Checkout"
+import BookingSuccess from "./components/booking/BookingSuccess"
+import Bookings from "./components/booking/Bookings"
+import FindBooking from "./components/booking/FindBooking"
+
 
 function App() {
+	return (
+			<main>
+				<Router>
+					<NavBar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/edit-room/:roomId" element={<EditRoom />} />
+						<Route path="/existing-rooms" element={<ExistingRooms />} />
+						<Route path="/add-room" element={<AddRoom />} />
 
-  return (
-    <div>
-      <Navbar />
-      <BrowserRouter>
-      <Routes>
-      <Route path='/home' exact Component={Homescreen} />
-      </Routes>
-      </BrowserRouter>
-    </div>
+						<Route
+							path="/book-room/:roomId"
+							element={
+									<Checkout />
+							}
+						/>
+						<Route path="/browse-all-rooms" element={<RoomListing />} />
 
-    
-  )
+						<Route path="/admin" element={<Admin />} />
+						<Route path="/booking-success" element={<BookingSuccess />} />
+						<Route path="/existing-bookings" element={<Bookings />} />
+						<Route path="/find-booking" element={<FindBooking />} />
+						<Route path="/logout" element={<FindBooking />} />
+					</Routes>
+				</Router>
+				<Footer />
+			</main>
+	)
 }
 
 export default App
