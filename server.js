ac428b8187f71a568fc8e22970519ac84819a622
Ujjwal Lamichhane <47378416+Uzzal644@users.noreply.github.com>
2024-01-30 12:59:26 +0545

    made changes

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require("express");
 const app = express();
 
 const dbConfig = require('./db')
+
 const roomsRoute = require('./routes/roomsRoute')
 
 app.use('/api/rooms', roomsRoute)
+app.use(express.json())
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Node server started using Nodemon`));
+
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:5173', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
